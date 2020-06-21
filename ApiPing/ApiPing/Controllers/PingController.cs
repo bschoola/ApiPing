c5035077b4222d223628100bd42051928e1b8450
brunoschoolaalb <bruno.schoola-sc@airliquide.com>
2020-06-21 11:53:50 -0300

    PingController

---
+++ ApiPing/ApiPing/Controllers/PingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Ping()
+        {
+            return Ok("Pong!");
+        }
+    }
+}
